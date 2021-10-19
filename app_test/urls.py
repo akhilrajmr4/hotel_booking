d40f58392e479aab9785e58b0c9a467f4af7f5be
akhil raj m r <akhilrajmrcreatif@gmail.com>
2021-10-19 19:53:28 +0530

    update, delete hotel

--- app_test/urls.py
+++ app_test/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('adminHome/', views.adminHome, name="adminHome"), # adminHome page (Akhilraj m r)
-    path('addhotel/', views.addHotel, name="addHotel"), # adminHome page (Akhilraj m r)
-    path('viewHotel/', views.viewHotel, name="viewHotel"), # adminHome page (Akhilraj m r)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('adminHome/', views.adminHome, name="adminHome"),  # adminHome page (Akhilraj m r)
+                  path('addhotel/', views.addHotel, name="addHotel"),  # addhotel page (Akhilraj m r)
+                  path('viewHotel/', views.viewHotel, name="viewHotel"),  # view hotel page (Akhilraj m r)
+                  path('viewhotels/delete_hotel/', views.delete_hotel, name="delete_hotel"),  # delete hotel (akhil
+                  # raj mr )
+                  path('viewhotels/updatehotel/<int:pk>/', views.updatehotel, name="updatehotel"),  # update hotel (akhil raj mr)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_test/views.py
+++ app_test/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 import random
 
@@ -11,21 +14,27 @@ def adminHome(request):
 
 # add hotel
 
-
-
 def addHotel(request):
     if request.method == "POST":
         hotel_name = request.POST.get('hotel_name', None)
         email = request.POST.get('email', None)
         phone = request.POST.get('phone', None)
         address = request.POST.get('address', None)
-        image_name = "icon-hotel.png"
-        password = random.SystemRandom().randint(100000, 999999)
-        hotel_details.objects.create(Hotel_name=hotel_name, Email=email, Phone_number=phone, Password=password,
-                                     Address =address, Image=image_name)
-        
-        msg = "Hotel added successfully"
-        return render(request, 'addhotel.html', {'msg': msg})
+        image_name = "/static/images/icon-hotel.png"
+
+        hotel_ext = hotel_details.objects.filter(Hotel_name=hotel_name).exists()
+        hotel_ext1 = hotel_details.objects.filter(Email=email).exists()
+
+        if hotel_ext or hotel_ext1:
+            exit = "Hotel already exist"
+            return render(request, 'addhotel.html', {'exit': exit})
+        else:
+            password = random.SystemRandom().randint(100000, 999999)
+            hotel_details.objects.create(Hotel_name=hotel_name, Email=email, Phone_number=phone, Password=password,
+                                         Address=address, Image=image_name)
+
+            msg = "Hotel added successfully"
+            return render(request, 'addhotel.html', {'msg': msg})
     else:
         return render(request, 'addhotel.html')
 
@@ -33,4 +42,45 @@ def addHotel(request):
 # view hotel
 
 def viewHotel(request):
-    return render(request, 'viewhotel.html')
+    hotel_det = hotel_details.objects.all().order_by('-id')
+    return render(request, 'viewhotel.html', {'hotel_det': hotel_det})
+
+
+# delete hotel
+
+def delete_hotel(request):
+    delete_id = request.GET.get('id')
+    del_data = hotel_details.objects.get(id=delete_id)
+    del_data.delete()
+    msg = "Hotel deleted"
+    return HttpResponse(json.dumps({'msg': msg}))
+
+
+# update hotel
+
+def updatehotel(request, pk):
+    if request.method == "POST":
+
+        hotel_data = hotel_details.objects.get(id=pk)
+
+        hotel_name = request.POST.get('hotel_name', None)
+        email = request.POST.get('email', None)
+
+        hotel_ext = hotel_details.objects.filter(Hotel_name=hotel_name).exists()
+        hotel_ext1 = hotel_details.objects.filter(Email=email).exists()
+
+        if hotel_ext or hotel_ext1:
+            exits = "Already exist"
+            return render(request, 'updatehotel.html', {'exits': exits})
+        else:
+            hotel_data.Hotel_name = hotel_name
+            hotel_data.Email = email
+            hotel_data.Phone_number = request.POST.get('phone', None)
+            hotel_data.Address = request.POST.get('address', None)
+            hotel_data.save()
+            msg = "Hotel update successfully"
+            return render(request, 'updatehotel.html', {'msg': msg})
+
+    else:
+        hotel_data = hotel_details.objects.get(id=pk)
+        return render(request, 'updatehotel.html', {'hotel_data': hotel_data})
